a44bfc1bf8bf998a014fcec259afc00e1aebf4bb
Harsh Jain <hjain5164@gmail.com>
2019-03-24 21:01:47 +0530

    Script to get the names of customers who reviewed a product on Amazon

---
+++ amazon_product_reviewers_name.js
@@ -0,0 +1,10 @@
+// Open any product reviews page.
+// Example : https://www.amazon.in/Amazon-Echo-Smart-speaker-Powered/product-reviews/B0725W7Q38/ref=cm_cr_dp_d_show_all_top?ie=UTF8&reviewerType=all_reviews
+
+
+var t = document.getElementsByClassName("a-profile-name");
+
+
+//Print the names of the customers who reviewed the product
+for(var i=0; i<t.length; i++)
+  console.log(t[i].textContent);
