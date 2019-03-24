d12cba1ba2d512c54ad6baf6b5dd5db37d2a1cb9
Harsh Jain <hjain5164@gmail.com>
2019-03-24 21:10:55 +0530

    Script to get all the reviews on Amazon.in

---
+++ amazon_product_reviews.js
@@ -0,0 +1,9 @@
+// Open any product reviews page.
+// Example : https://www.amazon.in/Amazon-Echo-Smart-speaker-Powered/product-reviews/B0725W7Q38/ref=cm_cr_dp_d_show_all_top?ie=UTF8&reviewerType=all_reviews
+//Run the code in the browser's console
+
+var reviews = document.getElementsByClassName("a-size-base review-text review-text-content");
+
+//Print all the reviews on the web page
+for(let i = 0; i<x.length; i++)
+  console.log(x[i].textContent);
